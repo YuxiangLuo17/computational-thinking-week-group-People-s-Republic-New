0eb9fc42f7d5d4faa395667e00a122a04c56781b
YuxiangLuo17 <luo.luo3@student.uva.nl>
2025-09-01 13:13:37 +0200

    Create git_exercise.py

---
+++ git_exercise.py
@@ -0,0 +1,16 @@
+import luo      
+import maciej      
+import bernardo
+import henning
+import janekl
+
+def team_intro():
+    print("This is Team YourTeamNameHere. We are:")
+    print(luo.my_name())
+    print(maciej.my_name())
+    print(bernardo.my_name())
+    print(henning.my_name())
+    print(janekl.my_name())
+
+if __name__ == "__main__":
+    team_intro()
